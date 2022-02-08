7f0e411e09eb992ba868e26459390d4b616f632a
gabriel rossier <rossier.gabriel218@gmail.com>
2022-02-08 10:35:30 +0100

    Added database port to .env

--- src/Db.js
+++ src/Db.js
@@ -3,17 +3,11 @@ class Db {
     constructor(){
         var env = process.env 
         // Option 3: Passing parameters separately (other dialects)
-        console.log(env);
         const sequelize = new Sequelize(env.DATABASE_NAME, env.DATABASE_USERNAME, env.DATABASE_PASSWORD, {
             host: 'localhost',
-            dialect: 'mariadb'
+            dialect: 'mariadb',
+            port: env.DATABASE_PORT
         });
-        try {
-            sequelize.authenticate();
-            console.log('Connection has been established successfully.');
-          } catch (error) {
-            console.error('Unable to connect to the database:', error);
-          }
     }
 }
 
