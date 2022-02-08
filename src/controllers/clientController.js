dc1008df76067783ac161a4fc441e6d970b0dbac
SCHOUWEY Zaid-Francois <zaid-francois.schouwey@cpnv.ch>
2022-02-08 14:39:45 +0100

    Change function name

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -4,6 +4,6 @@ const Sequelize = require('sequelize');
 var DataTypes = require("sequelize").DataTypes;
 console.log("bruh")
 
-exports.index = function(req, res){
+exports.allClients = function(req, res){
     Client.findAll().then((value)=>{res.status(200).json(JSON.stringify(value))}).catch(()=>res.status("502"));
 }
