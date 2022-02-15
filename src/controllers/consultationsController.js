f78debef73ed88111dd57156f028018d22768829
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2022-02-15 11:50:24 +0100

    Refactor

--- src/controllers/consultationsController.js
+++ src/controllers/consultationsController.js
@@ -13,9 +13,9 @@ exports.getAll = async (req, res) => {
 exports.getById = async (req, res) => {
   try {
     const id = req.params.id;
-    const consultations = await Consultation.findByPk(id);
+    const consultation = await Consultation.findByPk(id);
 
-    res.status(200).json(consultations);
+    res.status(200).json(consultation);
   } catch (e) {
     res.status(404).json({ error: "An error occured." });
   }
