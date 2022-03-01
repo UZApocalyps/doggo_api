a6361cbfd12ee6d711b4bfdd43881035a507f851
Schouwey <zaid.schouwey@gmail.com>
2022-03-01 13:50:05 +0100

    Swagger

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 require("./src/models/init-models");
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json')
+
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -8,11 +11,13 @@ const port = 3000;
 const consultationsRouter = require("./src/routes/consultationsRouter");
 const clientRouter = require("./src/routes/clientRouter");
 
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
 app.use(express.json()); // In order to parse requests json body
 
 app.use("/consultations", consultationsRouter);
 app.use("/client", clientRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
