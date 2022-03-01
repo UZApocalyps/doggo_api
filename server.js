011c0d1d4d0489177d2c5194d3c030383d7cf5ee
Gabriel Rossier <rossier.gabriel218@gmail.com>
2022-03-01 11:10:46 +0100

    Rename client to clients

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ const port = 3000;
 // Routers
 const consultationsRouter = require("./src/routes/consultationsRouter");
 const clientRouter = require("./src/routes/clientRouter");
+const dogRouter = require("./src/routes/dogRouter");
 
 app.use(express.json()); // In order to parse requests json body
 
 app.use("/consultations", consultationsRouter);
-app.use("/client", clientRouter);
+app.use("/clients", clientRouter);
+app.use("/dogs", dogRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
