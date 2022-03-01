04e04b77c4d7c11440c36e4f50bd5efd3510215d
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2022-03-01 14:30:24 +0100

    Add database sql files

---
+++ scripts/CynoClientDB/CategoriesChien.sql
@@ -0,0 +1,15 @@
+-- -----------------------------------------------------
+-- Insert `Cynology`.`categories`
+-- -----------------------------------------------------
+
+INSERT INTO categories VALUES
+(1, "Chiens de berger et de bouvier"),
+(2, "Chiens de type Pinscher et Schnauzer - Molossoïdes - Chiens de montagne et de bouvier suisses et Autres races"),
+(3, "Terriers"),
+(4, "Teckels"),
+(5, "Chiens de type Spitz et de type primitif"),
+(6, "Chiens courants, Chiens de recherche au sang et races apparentées"),
+(7, "Chiens d’arrêt"),
+(8, "Chiens rapporteurs de gibier - Chiens leveurs de gibier - Chiens d’eau"),
+(9, "Chiens d’agrément et de compagnie"),
+(10, "Lévriers");
