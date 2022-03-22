9655f86b9256a356c1f7822b332ee289f2c94c4f
Gabriel Rossier <rossier.gabriel218@gmail.com>
2022-03-22 14:23:29 +0100

    Added API Authentication

--- server.js
+++ server.js
@@ -16,6 +16,19 @@ const categoriesRouter = require("./src/routes/categoriesRouter");
 const breedRouter = require("./src/routes/breedRouter");
 const servicesRouter = require("./src/routes/servicesRouter");
 
+
+var auth = function (req, res, next) {
+  if(process.env.SECRET_TOKEN == req.headers["token"])
+  {
+    next();
+  }
+  else{
+    res.sendStatus(403);
+    next();
+  }
+};
+
+app.use(auth);
 app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(express.json()); // In order to parse requests json body
